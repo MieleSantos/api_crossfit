df71ddd7846cebf962bed51e75227111529cedd5
mielesantos <mielesnts@gmail.com>
2024-06-29 14:43:06 -0300

    add schemas/models centro_treinamento

---
+++ api/centro_treinamento/schemas.py
@@ -0,0 +1,30 @@
+from typing import Annotated
+from pydantic import Field
+from contrib.schemas import BaseSchema
+
+
+class CentroTreinamento(BaseSchema):
+
+    nome: Annotated[
+        str,
+        Field(
+            description="Nome do centro de treinamento",
+            example="Ct King",
+            max_length=20,
+        ),
+    ]
+
+    endereco: Annotated[
+        str,
+        Field(
+            description="Endereco do centro de treinamento",
+            example="rua do teste",
+            max_length=60,
+        ),
+    ]
+    proprietario: Annotated[
+        str,
+        Field(
+            description="Dono do centro de treinamento", example="Joao", max_digits=60
+        ),
+    ]
