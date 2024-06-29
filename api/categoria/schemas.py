de5c247ec88c1ed8df3c2c7120ffdd0a380e57dd
mielesantos <mielesnts@gmail.com>
2024-06-29 14:25:59 -0300

    add shemas/models categoria

---
+++ api/categoria/schemas.py
@@ -0,0 +1,10 @@
+from typing import Annotated
+
+from pydantic import Field
+from contrib.schemas import BaseSchema
+
+
+class Categoria(BaseSchema):
+    nome: Annotated[
+        str, Field(description="Nome da Categoria", examples="Scale", max_length=10)
+    ]
