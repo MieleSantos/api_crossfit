037f5081e66182693c068f015ba5bb6677fcc0e8
mielesantos <mielesnts@gmail.com>
2024-06-30 19:43:37 -0300

    add alembic

--- api/categoria/models.py
+++ api/categoria/models.py
@@ -4,8 +4,8 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class CategoriaModel(BaseModel):
-    __tablename__ = "atletas"
+    __tablename__ = 'atletas'
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(10), unique=True, nullable=False)
-    atleta: Mapped["AtletaModel"] = relationship(back_populates="categoria")
+    atleta: Mapped['AtletaModel'] = relationship(back_populates='categoria')
