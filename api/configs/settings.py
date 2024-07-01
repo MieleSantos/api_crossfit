d1ae096a88a74f9b36ec663bde3a9533d0044a9c
mielesantos <mielesnts@gmail.com>
2024-07-01 16:00:49 -0300

    add conexao com banco

--- api/configs/settings.py
+++ api/configs/settings.py
@@ -3,4 +3,9 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_URL: str = Field()
+    DB_URL: str = Field(
+        default="postgresql+asyncpg://workout:workout@172.18.111.176/workout"
+    )
+
+
+settings = Settings()
