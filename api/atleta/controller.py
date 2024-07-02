dd3a3d7aa0d437c9287ee44d5f05fa535ab21e82
mielesantos <mielesnts@gmail.com>
2024-07-02 16:13:04 -0300

    add rota post de atleta

--- api/atleta/controller.py
+++ api/atleta/controller.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Body, status
-
+from uuid import uuid4
 from api.atleta.schemas import AtletaIn, AtletaOut
 from api.contrib.dependencies import data_base_dependecy
+from api.atleta.models import AtletaModel
 
 router = APIRouter()
 
@@ -12,5 +13,9 @@ router = APIRouter()
     status_code=status.HTTP_201_CREATED,
     response_model=AtletaOut,
 )
-async def post(db_session: data_base_dependecy, atleta: AtletaIn = Body(...)):
-    pass
+async def post(db_session: data_base_dependecy, atleta_in: AtletaIn = Body(...)):
+    atleta_out = AtletaOut(id=uuid4(), **atleta_in.model_dump())
+    atleta_model = AtletaModel(**atleta_out.model_dump())
+    db_session.add(atleta_model)
+    await db_session.commit()
+    return atleta_out
